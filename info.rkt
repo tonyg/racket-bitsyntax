1666c79f18dd19b99589ecb4042cf894387a0d53
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2011-06-28 23:19:24 -0400

    info.rkt

---
+++ info.rkt
@@ -0,0 +1,14 @@
+#lang setup/infotab
+(define name "bitsyntax")
+(define blurb
+  (list
+   `(p "Erlang-like binary pattern-matching and bit string manipulation for Racket")))
+(define release-notes
+  (list
+   `(ul (li "Initial release"))))
+(define categories '(net))
+(define can-be-loaded-with 'all)
+(define homepage "https://github.com/tonyg/racket-bitsyntax")
+(define primary-file "main.rkt")
+(define repositories '("4.x"))
+(define scribblings '(("manual.scrbl" ())))
