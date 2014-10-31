9b8023646fbf807f31f845cdf25a11598aa00ef8
Leif Andersen <leif@leifandersen.net>
2014-10-31 17:04:34 -0400

    Fixed dependencies.

--- info.rkt
+++ info.rkt
@@ -1,2 +1,7 @@
 #lang setup/infotab
 (define collection 'multi)
+(define deps '("base"
+               "rackunit-lib"
+               "typed-racket-lib"
+               "typed-racket-more"))
+(define build-deps '("scribble-lib" "racket-doc"))
